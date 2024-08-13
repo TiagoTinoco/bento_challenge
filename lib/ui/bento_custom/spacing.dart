7bdc97f5e679c1a47ad8860e6e9ee6d7a2465fd9
TiagoTinoco <tiagotinocomartinsdossantos@gmail.com>
2024-08-12 21:56:16 -0300

    increment fonts and the bento custom widgets

---
+++ lib/ui/bento_custom/spacing.dart
@@ -0,0 +1,25 @@
+abstract class BentoSpacing {
+  /// The extra-extra-extra small spacing value.
+  static double xxxs = 4.0;
+
+  /// The extra-extra small spacing value.
+  static double xxs = 8.0;
+
+  /// The extra-extra small spacing value.
+  static double xs = 16.0;
+
+  /// The small spacing value.
+  static double sm = 24.0;
+
+  /// The medium spacing value.
+  static double md = 32.0;
+
+  /// The large spacing value.
+  static double lg = 40.0;
+
+  /// The extra large spacing value.
+  static double xl = 48.0;
+
+  /// The extra-extra large spacing value.
+  static double xxl = 64.0;
+}
---
+++ lib/ui/bento_custom/text.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/widgets.dart';
+
+class BentoTextStyles {
+  static const TextStyle captionDF = TextStyle(fontSize: 15, height: 1.25, letterSpacing: 0.4);
+  static const TextStyle bodyDF = TextStyle(fontSize: 18, height: 1.5, letterSpacing: 0);
+  static const TextStyle subtitle = TextStyle(fontSize: 22, height: 1.25, letterSpacing: 0);
+  static const TextStyle title = TextStyle(fontSize: 28, height: 1.25, letterSpacing: 0);
+}
+
+class BentoTextCaptionDF extends BentoText {
+  const BentoTextCaptionDF(
+    String data, {
+    super.key,
+    super.style,
+    super.strutStyle,
+    super.textAlign,
+    super.textDirection,
+    super.locale,
+    super.softWrap,
+    super.overflow,
+    super.textScaler,
+    super.selectionColor,
+    super.maxLines,
+    super.semanticsLabel,
+    super.textWidthBasis,
+    super.textHeightBehavior,
+  }) : super(data, BentoTextStyles.captionDF);
+}
+
+class BentoTextBodyDF extends BentoText {
+  const BentoTextBodyDF(
+    String data, {
+    super.key,
+    super.style,
+    super.strutStyle,
+    super.textAlign,
+    super.textDirection,
+    super.locale,
+    super.softWrap,
+    super.overflow,
+    super.textScaler,
+    super.selectionColor,
+    super.maxLines,
+    super.semanticsLabel,
+    super.textWidthBasis,
+    super.textHeightBehavior,
+  }) : super(data, BentoTextStyles.bodyDF);
+}
+
+class BentoTextSubtitle extends BentoText {
+  const BentoTextSubtitle(
+    String data, {
+    super.key,
+    super.style,
+    super.strutStyle,
+    super.textAlign,
+    super.textDirection,
+    super.locale,
+    super.softWrap,
+    super.overflow,
+    super.textScaler,
+    super.selectionColor,
+    super.maxLines,
+    super.semanticsLabel,
+    super.textWidthBasis,
+    super.textHeightBehavior,
+  }) : super(data, BentoTextStyles.subtitle);
+}
+
+class BentoTextTitle extends BentoText {
+  const BentoTextTitle(
+    String data, {
+    super.key,
+    super.style,
+    super.strutStyle,
+    super.textAlign,
+    super.textDirection,
+    super.locale,
+    super.softWrap,
+    super.overflow,
+    super.textScaler,
+    super.selectionColor,
+    super.maxLines,
+    super.semanticsLabel,
+    super.textWidthBasis,
+    super.textHeightBehavior,
+  }) : super(data, BentoTextStyles.title);
+}
+
+class BentoText extends StatelessWidget {
+  const BentoText(
+    this.data,
+    this.defaultStyle, {
+    super.key,
+    this.style,
+    this.strutStyle,
+    this.textAlign,
+    this.textDirection,
+    this.locale,
+    this.softWrap,
+    this.overflow,
+    this.textScaler,
+    this.selectionColor,
+    this.maxLines,
+    this.semanticsLabel,
+    this.textWidthBasis,
+    this.textHeightBehavior,
+  });
+
+  final String data;
+  final TextStyle defaultStyle;
+  final TextStyle? style;
+  final StrutStyle? strutStyle;
+  final TextAlign? textAlign;
+  final TextDirection? textDirection;
+  final Locale? locale;
+  final bool? softWrap;
+  final TextOverflow? overflow;
+  final TextScaler? textScaler;
+  final Color? selectionColor;
+  final int? maxLines;
+  final String? semanticsLabel;
+  final TextWidthBasis? textWidthBasis;
+  final TextHeightBehavior? textHeightBehavior;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      data,
+      style: defaultStyle.merge(style),
+      strutStyle: strutStyle,
+      textAlign: textAlign,
+      textDirection: textDirection,
+      locale: locale,
+      softWrap: softWrap,
+      overflow: overflow,
+      textScaler: textScaler,
+      selectionColor: selectionColor,
+      maxLines: maxLines,
+      semanticsLabel: semanticsLabel,
+      textWidthBasis: textWidthBasis,
+      textHeightBehavior: textHeightBehavior,
+    );
+  }
+}
