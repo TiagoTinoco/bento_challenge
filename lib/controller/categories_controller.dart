0617930edb24fcf98e5a2be7504694a7e9813bf4
TiagoTinoco <tiagotinocomartinsdossantos@gmail.com>
2024-08-17 18:12:16 -0300

    put extends change notifier and in products create getter, setter and new function get by id

--- lib/controller/categories_controller.dart
+++ lib/controller/categories_controller.dart
@@ -4,15 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:bento_challenge/models/category_model.dart';
 import 'package:bento_challenge/repository/categories_repository.dart';
 
-abstract class LocalCategoriesRepository {
+abstract class _LocalCategoriesRepository {
+  /// get the categories of repository folder
   Future<List<CategoryModel>> getCategories();
 }
 
 /// This class was created to simulate a data request.
-class CategoriesController extends ChangeNotifier implements LocalCategoriesRepository {
+class CategoriesController extends ChangeNotifier implements _LocalCategoriesRepository {
   @override
-
-  /// get the categories of repository folder
   Future<List<CategoryModel>> getCategories() async {
     await Future.delayed(Duration(milliseconds: 1600));
 
